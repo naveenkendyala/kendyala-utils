15323ec56ea2c8f8b92cd4aefde7c5ead7257fda
Naveen Kendyala <naveenkendyala@kendyala.com>
2020-01-26 22:34:47 -0500

    updated hpa & scripts

--- 00.provision.cluster.gke/00.steps.sh
+++ 00.provision.cluster.gke/00.steps.sh
@@ -3,15 +3,15 @@
 ####################################################################################
 
 #01 : Check Python Version (2.7)
-python -V
+#python -V
 
 #02 : Download gcloud sdk
 
 #03 : Install the SDK
-./google-cloud-sdk/install.sh
+#./google-cloud-sdk/install.sh
 
 #04 : Initialize the environment
-gcloud init 
+#gcloud init 
 
 #05 : Create GKE cluster
 
@@ -23,31 +23,34 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-b
 
 #08 : Create Role
 #    Apply the dashboard-sa.yaml
-kubectl apply -f dashboard-sa.yaml
+kubectl apply -f /Users/naveenkumarkendyala/00.professional/01.workspaces/2020.git.repos/kendyala-utils/00.provision.cluster.gke/02.dashboard-sa.yaml
 
 #09 : Create ROLE BINDING
 #    Apply the dashboard-rolebinding.yaml
-kubectl apply -f dashboard-rolebinding.yaml
+kubectl apply -f /Users/naveenkumarkendyala/00.professional/01.workspaces/2020.git.repos/kendyala-utils/00.provision.cluster.gke/03.dashboard-rolebinding.yaml
 
 #10 : Get the Token
-kubectl proxy
-kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+#kubectl proxy
+#kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
 
 
 #11 : Access the Cluster
-http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+#http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 
 #12 : Deploy the Services
 
 #13 : Enable Firewall to allow traffic
 #gcloud compute firewall-rules create <insert_rule_name> --allow tcp:[NODE_PORT]
 #gcloud compute firewall-rules create allow-mysql --allow tcp:30100
-#gcloud compute firewall-rules create allow-quarkus-native --allow tcp:30202
-#gcloud compute firewall-rules create allow-quarkus-jvm --allow tcp:30200
+
+#gcloud compute firewall-rules create allow-eap-jvm --allow tcp:30200
 #gcloud compute firewall-rules create allow-spring-jvm --allow tcp:30201
+#gcloud compute firewall-rules create allow-quarkus-native --allow tcp:30203
+#gcloud compute firewall-rules create allow-quarkus-jvm --allow tcp:30202
 
 
 #gcloud compute firewall-rules delete allow-mysql
 #gcloud compute firewall-rules delete allow-quarkus-jvm
 #gcloud compute firewall-rules delete allow-quarkus-native
 #gcloud compute firewall-rules delete allow-spring-jvm
+#gcloud compute firewall-rules delete allow-eap-jvm
---
+++ 00.provision.databases/01.mysql/00.provision.sh
@@ -0,0 +1,3 @@
+kubectl create ns quarkus-demo
+kubectl config set-context --current --namespace quarkus-demo
+kubectl apply -f /Users/naveenkumarkendyala/00.professional/01.workspaces/2020.git.repos/kendyala-utils/00.provision.databases/01.mysql
