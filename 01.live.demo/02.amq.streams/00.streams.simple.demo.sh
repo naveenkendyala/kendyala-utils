6e3316827659e298a011e8f0d470eaf852806d1a
Naveen Kendyala <subscribeken@gmail.com>
2020-05-11 18:24:36 -0400

    draft of amq-streams examples and demo

---
+++ 01.live.demo/02.amq.streams/00.streams.simple.demo.sh
@@ -0,0 +1,38 @@
+#Provision OCP Environment
+
+#Create new OCP Project
+oc new-project myproject
+
+#Install the Streams Operator
+
+#Install the Kafka Cluster with "listener" configuration to external type as "route"
+oc apply -f 01.kafka-persistent.yaml
+
+#Create a Topic
+oc apply -f 02.topic.yaml
+
+#Get the routes
+oc get routes my-cluster-kafka-bootstrap -o=jsonpath='{.status.ingress[0].host}{"\n"}'
+
+#Get the ca cert and imnport into trusted certs
+oc extract secret/my-cluster-cluster-ca-cert --keys=ca.crt --to=- > ca.crt
+keytool -import -trustcacerts -alias root -file ca.crt -keystore truststore.jks -storepass password -noprompt
+
+
+#Send a Message to Topic
+#***** CHANGE THE BROKER LIST *****
+kafka_2.12-2.4.0.redhat-00005/bin/kafka-console-producer.sh \
+--broker-list my-cluster-kafka-bootstrap-amq-streams-pg.apps.cluster-91fc.91fc.example.opentlc.com:443 \
+--producer-property security.protocol=SSL \
+--producer-property ssl.truststore.password=password \
+--producer-property ssl.truststore.location=truststore.jks \
+--topic my-topic
+
+#Receive a Message
+#***** CHANGE THE BOOTStRAP LIST *****
+kafka_2.12-2.4.0.redhat-00005/bin/kafka-console-consumer.sh \
+--bootstrap-server my-cluster-kafka-bootstrap-amq-streams-pg.apps.cluster-91fc.91fc.example.opentlc.com:443 \
+--consumer-property security.protocol=SSL \
+--consumer-property ssl.truststore.password=password \
+--consumer-property ssl.truststore.location=truststore.jks \
+--topic my-topic --from-beginning
